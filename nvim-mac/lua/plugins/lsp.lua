09670f9cf619a3a1e4706406f336c093148a47ec
Ivan Majeru <ivanm@reco.ai>
2025-06-27 17:51:51 +0300

    add more configuration for null_ls

--- nvim-mac/lua/plugins/lsp.lua
+++ nvim-mac/lua/plugins/lsp.lua
@@ -29,7 +29,26 @@ return {
         sources = {
           null_ls.builtins.formatting.stylua,
           null_ls.builtins.formatting.prettier,
+          null_ls.builtins.formatting.markdownlint,
+
+          -- diagnostics
           null_ls.builtins.diagnostics.staticcheck,
+          null_ls.builtins.diagnostics.golangci_lint,
+          null_ls.builtins.diagnostics.markdownlint,
+          null_ls.builtins.diagnostics.buf,
+          null_ls.builtins.diagnostics.codespell,
+
+          -- code_action
+          null_ls.builtins.code_actions.gitsigns,
+          null_ls.builtins.code_actions.refactoring,
+          null_ls.builtins.code_actions.impl,
+
+          -- completion
+          null_ls.builtins.completion.nvim_snippets,
+
+          -- hover
+          null_ls.builtins.hover.dictionary,
+          null_ls.builtins.hover.printenv,
         },
         diagnostic_on_save = {
         },
